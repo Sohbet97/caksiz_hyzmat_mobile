fa2077462915c8f1d9eab2cd7b95c49f3ca35f4e
Sohbet <maksadow.sohbet97@gmail.com>
2026-07-27 14:03:40 +0300

    created load metod

--- lib/features/schools/data/repositories/schools_repository.dart
+++ lib/features/schools/data/repositories/schools_repository.dart
@@ -1,7 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:mobile/features/schools/data/models/school_model.dart';
 
 class SchoolsRepository {
   final Dio dio;
 
   SchoolsRepository({required this.dio});
+
+  Future<List<SchoolModel>> loadSchools() async {
+    return [];
+  }
 }
